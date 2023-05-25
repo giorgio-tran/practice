e56a77120076cbf54b11d1ac20531b51a0492479
Giorgio <tran.giorgio@gmail.com>
2023-05-25 00:01:02 -1000

    Initial commit

---
+++ bobaicecream/bobaicecream/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <Link href="/test">Test</Link>
+      </div>
+    </main>
+  )
+}
---
+++ bobaicecream/bobaicecream/src/app/pages/page.tsx
@@ -0,0 +1,10 @@
+const Hello = () => {
+
+  return (
+    <div>
+      Hello world!
+    </div>
+  )
+}
+
+export default Hello;
