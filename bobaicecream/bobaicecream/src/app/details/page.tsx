5b37249324678960e9a519691e80b1ef11448438
Giorgio <tran.giorgio@gmail.com>
2023-06-25 19:03:59 -1000

    Test requestVideoCallBack to draw canvases

--- bobaicecream/bobaicecream/src/app/details/page.tsx
+++ bobaicecream/bobaicecream/src/app/details/page.tsx
@@ -1,39 +1,81 @@
 "use client";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 
 export default function FilePreviewer() {
-  const [imagePreview, setImagePreview] = useState<string>("");
   const [videoPreview, setVideoPreview] = useState<string>("");
+  const [startTime, setStartTime] = useState<number>(0);
+  const layerRef = useRef<number>(1);
   const filePicekerRef = useRef<HTMLInputElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const drawingRef = useRef<HTMLCanvasElement>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const startRef = useRef<boolean>(true);
   function previewFile(e: React.ChangeEvent<HTMLInputElement>) {
-    // Reading New File (open file Picker Box)
-    const reader = new FileReader();
-    // Gettting Selected File (user can select multiple but we are choosing only one)
     const selectedFile = e.target.files?.[0];
     if (selectedFile) {
-      reader.readAsDataURL(selectedFile);
+      console.log(e.target.files);
+      setVideoPreview(URL.createObjectURL(selectedFile));
     }
-    // As the File loaded then set the stage as per the file type
-    reader.onload = (readerEvent) => {
-      if (selectedFile?.type.includes("image")) {
-        setImagePreview((readerEvent.target as FileReader).result as string);
-      } else if (selectedFile?.type.includes("video")) {
-        setVideoPreview((readerEvent.target as FileReader).result as string);
-      }
-    };
   }
-  
+
+  function drawCanvas(now: DOMHighResTimeStamp, metadata: VideoFrameCallbackMetadata) {
+      startRef.current = false;
+      console.log("metadata", metadata);
+      const video = videoRef.current;
+      if (video) {
+        if (canvasRef.current && drawingRef.current) {
+          canvasRef.current.height = video?.offsetHeight as number;
+          canvasRef.current.width = video?.offsetWidth as number;
+          const ctx = canvasRef.current?.getContext("2d");
+          ctx?.drawImage(
+            video as HTMLVideoElement,
+            0,
+            0,
+            video?.offsetWidth as number,
+            video?.offsetHeight as number
+          );
+          const drawContext = drawingRef.current?.getContext("2d") as CanvasRenderingContext2D;
+              for (let i = 0; i < video.offsetWidth; i++) {
+                const pixel = ctx?.getImageData(i, layerRef.current - 1, 1, 1).data as Uint8ClampedArray;
+                console.log("metadata", metadata)
+                drawContext.fillStyle = `rgba(${pixel[0]}, ${pixel[1]}, ${pixel[2]}, ${pixel[3]})`;
+                drawContext.fillRect(i, layerRef.current - 1 as number, 1, 1);
+              // console.log("layer:", layerRef.current);
+              }
+            }
+          layerRef.current += 1;
+          
+          video?.requestVideoFrameCallback(drawCanvas)
+      }
+  }
+
   return (
     <div>
       <h1>Preview Image/Video</h1>
       <div className="btn-container">
-        <input ref={filePicekerRef} accept="image/*, video/*" onChange={previewFile} type="file" hidden />
-        <button className="btn" onClick={() => filePicekerRef.current?.click()}>Choose</button>
+        <input
+          ref={filePicekerRef}
+          accept="image/*, video/*"
+          onChange={previewFile}
+          type="file"
+          hidden
+        />
+        <button className="btn" onClick={() => filePicekerRef.current?.click()}>
+          Choose
+        </button>
         <button className="btn">x</button>
       </div>
-      <div className="preview">
-        <img src={imagePreview}></img>
-        <video controls src={videoPreview}></video>
+      <div style={{ display: "flex", flexDirection: "column", width: "500px" }}>
+        <button
+          onClick={() => {
+            videoRef.current?.paused
+              ? videoRef.current.play()
+              : videoRef.current?.pause();
+          }}
+        >PLAY</button>
+        <video controls src={videoPreview} ref={videoRef}  onPlay={drawCanvas}/>
+        <canvas ref={canvasRef} />
+        <canvas ref={drawingRef} height="2000px"/>
       </div>
     </div>
   );
